ee3e542d58e8d5e61dd9c1c19140a8efe1c3cdc8
Nikita Borovkov <borovckov@yandex.ru>
2021-11-09 23:34:49 +0300

    added basic layout with bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container class="mt-5">
+    <b-row class="justify-content-center">
+      <b-col xs="12" md="6">
+        <b-card tag="div">
+
+          <h3 style="text-align: center">Вызов врача на дом</h3>
+          <hr class="mb-5">
+
+          <p>Введите имя:</p>
+          <b-form-input id="fullName"
+                        type="text"
+                        v-model="fullName"
+                        class="mb-3"
+          ></b-form-input>
+
+          <p>Введите адрес:</p>
+          <b-form-input id="address"
+                        type="text"
+                        v-model="address"
+                        class="mb-3"
+          ></b-form-input>
+
+          <p>Выберите дату:</p>
+          <b-form-input id="appointmentDate"
+                        type="date"
+                        v-model="appointmentDate"
+                        :min="dateToday"
+                        class="mb-3"
+          ></b-form-input>
+
+          <b-button class="mt-3" @click="openModal" style="width: 100%" block variant="info">Записаться</b-button>
+
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {getTodayDate} from "../utils/dateHelper/dateHelper";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      fullName: ``,
+      address: ``,
+      appointmentDate: ``,
+      modalWindow: false,
+    }
+  },
+  computed: {
+    dateToday () {
+      return getTodayDate()
+    }
+  },
+  methods: {
+    openModal () {
+      this.modalWindow = true
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
