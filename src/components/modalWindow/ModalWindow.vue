0848e96604fe77c5fd8da6e0198a12320825a8b3
Nikita Borovkov <borovckov@yandex.ru>
2021-11-10 01:58:41 +0300

    created modalWindow component

---
+++ src/components/modalWindow/ModalWindow.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-modal id="responseModal">
+        <p class="my-4">{{fullName}}</p>
+        <p class="my-4">{{address}}</p>
+        <p class="my-4">{{appointmentDate}}</p>
+    </b-modal>
+</template>
+
+<script>
+    export default {
+        name: "ModalWindow",
+        props: {
+            fullName: String,
+            address: String,
+            appointmentDate: String,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
