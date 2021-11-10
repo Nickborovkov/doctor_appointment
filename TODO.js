20d329e3adf784cd162d848fc3db38d50fba885d
Nikita Borovkov <borovckov@yandex.ru>
2021-11-10 03:54:50 +0300

    added TODO.js file with tickets

---
+++ TODO.js
@@ -0,0 +1,7 @@
+//todo `store errors handling`
+//todo `split the app for better experience`
+//todo `maybe clear inputs after submit`
+//todo `showing address hints`
+//todo `showing errors with address hints`
+//todo `choosing address hints with arrows`
+//todo `translit cyrillic to latin (if it is cyrillic) for result string`
