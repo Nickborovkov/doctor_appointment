1ebc18543adb2aab10734f4bd9e17464c525e842
Nickborovkov <borovckov@yandex.ru>
2021-11-10 13:33:18 +0300

    added info string showing

--- src/components/modalWindow/ModalWindow.vue
+++ src/components/modalWindow/ModalWindow.vue
@@ -1,16 +1,21 @@
 <template>
     <b-modal id="responseModal" hide-header hide-footer ok-only >
+        <!--Basic text info-->
         <h3>Вы успешно вызвали врача</h3>
         <hr>
         <h4>Информация о вызове:</h4>
         <p class="my-4">Имя:<span style="color: #5bc0de">  {{fullName}}</span></p>
         <p class="my-4">Адрес:<span style="color: #5bc0de">  {{address}}</span></p>
         <p class="my-4">Дата:<span style="color: #5bc0de">  {{reverseDate}}</span></p>
+
+        <!--Result string as in example-->
         <h4>Сервисная информация:</h4>
-        <p class="my-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet aut blanditiis, consectetur consequuntur delectus dicta dolorem eligendi est facilis iusto molestiae nesciunt nisi nostrum nulla, odio omnis porro praesentium quam quos reiciendis rem saepe sit voluptas voluptatibus. Accusamus architecto aut error nemo omnis similique temporibus vel veniam! Dolor, eveniet.</p>
+        <p class="my-4">{{resultString}}</p>
+
+        <!--Close button-->
         <b-button variant="info"
                   style="width: 100%"
-                  @click="confirmAppointment">Close Me</b-button>
+                  @click="confirmAppointment">Готово</b-button>
     </b-modal>
 </template>
 
@@ -22,21 +27,18 @@
             address: String,
             appointmentDate: String,
             clearForm: Function,
+            resultString: String,
         },
         computed: {
             reverseDate () {
                 return this.appointmentDate.split(`-`).reverse().join(`.`)
-            }
+            },
         },
         methods: {
             confirmAppointment () {
                 this.$bvModal.hide('responseModal')
                 this.clearForm()
-            }
-        }
+            },
+        },
     }
 </script>
-
-<style scoped>
-
-</style>
